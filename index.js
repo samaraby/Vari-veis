b556aea08ff8672672699b30ed0a08437b6daebe
samaraby <samara.monteiro.miranda@escola.pr.gov.br>
2024-09-03 23:08:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const nomeCompleto = "Samara Byanca Monteiro Miranda ";
+const anoNascimento = 2007;
+const anoAtual = 2024;
+const idadeAtual = 17;
+const anoQueVem = 2025;
+const idade = 18;
+const sonho = "ser arquiteta "
+
+console.log(" olá eu sou " + nomeCompleto + " estamos no ano " + anoAtual + " eu nasci no ano de "  + anoNascimento  + " minha idade é " +  idadeAtual + " no ano que vem  " + anoQueVem + " eu terei " + idade + " ,e meu sonho é " + sonho );
+
+console.log(`oi eu sou ${nomeCompleto} meu ano de nascimento é ${anoNascimento} e estamos no ano de ${anoAtual} minha idade é ${idadeAtual} no ano que vem ${anoQueVem} eu terei ${idade} e meu sonho é ${sonho} `)
+
+const listaDeViagens = new Array(
+  `Japão`,
+  `Paris`,
+  `Canadá`,
+  `Estados Unidos`,
+  `Roma`
+);
+
+console.log(listaDeViagens);
