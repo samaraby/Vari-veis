3e4c0df751f37de60504c08beb729fb1ca1f6fbd
samaraby <samara.monteiro.miranda@escola.pr.gov.br>
2024-10-08 23:09:11 +0000

    08/10/24

--- index.js
+++ index.js
@@ -14,24 +14,29 @@ console.log(" olá eu sou " + nomeCompleto + " estamos no ano " + anoAtual + " e
 console.log(`oi eu sou ${nomeCompleto} meu ano de nascimento é ${anoNascimento} e estamos no ano de ${anoAtual} minha idade é ${idadeAtual} no ano que vem ${anoQueVem} eu terei ${idade} e meu sonho é ${sonho} `);
 
 const loginCerto = "samara"
-const senhaCerto = 123
-console.log("");
-console.log("=AGENCIA DE VIAGENS=");
-console.log("");
-console.log("faça seu login");
+const senhaCerto = 1704
+console.log("")
+console.log("                               ====================");
+console.log("                               =AGENCIA DE VIAGENS=");
+console.log("                               ====================");
+console.log("")
 
-var login= entrada("digite deu login:");
-var senha= entrada("digite sua senha:");
+console.log("+faça seu login+");
+console.log("")
+
+var login= entrada("*digite deu login:");
+var senha= entrada("*digite sua senha:");
 
 while(login != loginCerto) {
-  console.log("login errado");
-  login = entrada("digite seu login novamente");
+  console.log("=login errado=");
+  login = entrada("*digite seu login novamente*");
 }
 while(senha != senhaCerto) {
-  console.log("senha errado");
-  senha = entrada("digite sua senha novamente");
+  console.log("=senha errado=");
+  senha = entrada("*igite sua senha novamente*");
 }
 
+console.log("")
 const listaDeViagens = new Array(
   `Japão`,
   `Paris`,
@@ -42,22 +47,27 @@ const listaDeViagens = new Array(
 
 console.log(listaDeViagens);
 
+console.log("")
 console.log(listaDeViagens[3]);
 listaDeViagens.push("coronel vivida");
 console.log(listaDeViagens);
 
+console.log("")
 listaDeViagens.splice(3,1);
 console.log(listaDeViagens);
 
-var nomeComprador = entrada("qual o seu nome");;
-var idadeComprador = entrada("qual sua idade?");
+console.log("")
+var nomeComprador = entrada("*qual o seu nome?");;
+var idadeComprador = entrada("*qual sua idade?");
 
 if(idadeComprador >= 18){
-  
-  console.log("ja que voce é de maior, podemos vender as passagens.")
+
+  console.log("")
+  console.log("ja que voce é de maior, podemos vender as passagens para onde seria.")
 };
 
+console.log("")
 if (idadeComprador < 18){
-  console.log(`olá ${nomeComprador} qual seria seu destinos para eu poder ver aqui ser ainda temos passagens ${listaDeViagens} para sua sorte temos sim passagens disponivel no momento.
+  console.log(`olá ${nomeComprador} qual seria seu destinos para eu poder ver aqui ser ainda temos passagens? ${listaDeViagens} para sua sorte temos sim passagens disponivel no momento.
  `);
 };
